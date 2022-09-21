ba524a2175a898595c03b0719e5efbaf377a461f
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-09-21 07:19:14 +0000

    debug task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,11 +10,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp;
 
-	temp = malloc(sizeof(binary_tree_t));
-	if (temp == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	if (parent == NULL)
+	temp = malloc(sizeof(binary_tree_t));
+	if (temp == NULL)
 		return (NULL);
 
 	temp->n = value;
