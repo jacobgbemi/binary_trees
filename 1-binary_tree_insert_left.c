bf16ba9df2b364470a870594da3c774107189590
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-09-21 07:15:56 +0000

    debug task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,6 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		temp->left = parent->left;
 		parent->left->parent = temp;
+		parent->left = temp;
 	}
 	parent->left = temp;
 
